5c6229a88bc2d76e8c66426f1ed2ff9bf922504c
Alex Brown <ruokblah@gmail.com>
2023-04-30 14:48:14 -0400

    init

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
+// main.go
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
 
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	// This will join our previous block's relevant info with the new blocks
+	hash := sha256.Sum256(info)
+	//This performs the actual hashing algorithm
+	b.Hash = hash[:]
+	//If this ^ doesn't make sense, you can look up slice defaults
+}
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	//If this is gibberish to you look up
+	// pointer syntax in go
+	block.DeriveHash()
+	return block
+}
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
 func main() {
-	fmt.Println("Hello, World!")
+
+	chain := InitBlockChain()
+
+	chain.AddBlock("first block after genesis")
+	chain.AddBlock("second block after genesis")
+	chain.AddBlock("third block after genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("data: %s\n", block.Data)
+		fmt.Printf("hash: %x\n", block.Hash)
+	}
+
 }
